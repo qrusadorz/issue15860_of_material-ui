9fb70dca07ff9778414656aeac95714886afddc9
qrusadorz <qrusadorz@gmail.com>
2019-05-26 13:41:31 +0900

    Add a message for users in Japanese

--- src/App.js
+++ src/App.js
@@ -22,6 +22,13 @@ export default function App() {
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
+          ご利用されているユーザーの方へ
+        </Typography>
+        <Typography color="textSecondary" gutterBottom>
+          本ページは5/25から実施している内部問題解決のためのテストページです。6：00以降のアクセスでこのページが表示された場合、一度タブを閉じて再度アクセスしてください。
+        </Typography>
+        <div style={{marginBottom: 50}}/>
+        <Typography variant="h4" component="h1" gutterBottom>
           Create React App v4-beta example
         </Typography>
         <ProTip />
