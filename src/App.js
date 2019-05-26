49b59199023f09ed1ed63994bb8fb72e829333d5
qrusadorz <qrusadorz@gmail.com>
2019-05-26 14:51:55 +0900

    Delete messages for production environment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 import ProTip from './ProTip';
 import Link from '@material-ui/core/Link';
 
@@ -21,13 +22,6 @@ export default function App() {
   return (
     <Container maxWidth="sm">
       <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          ご利用されているユーザーの方へ
-        </Typography>
-        <Typography color="textSecondary" gutterBottom>
-          本ページは5/25から実施している内部問題解決のためのテストページです。6：00以降のアクセスでこのページが表示された場合、一度タブを閉じて再度アクセスしてください。
-        </Typography>
-        <div style={{marginBottom: 50}}/>
         <Typography variant="h4" component="h1" gutterBottom>
           Create React App v4-beta example
         </Typography>
